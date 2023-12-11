f2461503b70c8a1f3970f7c3ee04c9ee28b7e832
Matthew Joseph Fang Bilaos <bilaos02311@gmail.com>
2023-12-12 02:44:27 +0800

    Add: prediction-chart.php

---
+++ data-visualization/prediction-chart.php
@@ -0,0 +1,17 @@
+<?php
+$pageTitle = 'Prediction Charts';
+
+include './../includes/header.php';
+?>
+
+<div class="chartCard">
+
+    <div class="chartBox line">
+        <h2>Total Spend Every Month</h2>
+        <canvas id="ChartLine"></canvas>
+    </div>
+
+
+</div>
+
+<?php include './../includes/footer.php' ?>
