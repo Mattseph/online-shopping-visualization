de141bb3cfe5cbb8fd3c4a7cff15c9f6d59be356
Matthew Joseph Fang Bilaos <bilaos02311@gmail.com>
2023-12-11 14:11:13 +0800

    Add: Header and Footer

---
+++ includes/footer.php
@@ -0,0 +1,15 @@
+</div>
+
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/d3/7.8.5/d3.min.js" integrity="sha512-M7nHCiNUOwFt6Us3r8alutZLm9qMt4s9951uo8jqO4UwJ1hziseL6O3ndFyigx6+LREfZqnhHxYjKRJ8ZQ69DQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<script>
+    const chartData = './../assets/data/online-shopping-data.csv';
+</script>
+<script src="./.././assets/js/main.js"></script>
+</body>
+
+</html>
