4e8ed6c00bff2ced5243615a0f362d411b973925
Matthew Joseph Fang Bilaos <105849780+Mattseph@users.noreply.github.com>
2023-12-11 16:21:32 +0800

    Add: <h2>. Edit: canvas id='myChartLine -> ChartLine'

--- data-visualization/sales-chart.php
+++ data-visualization/sales-chart.php
@@ -7,8 +7,10 @@ include './../includes/header.php';
 <div class="chartCard">
 
     <div class="chartBox line">
-        <canvas id="myChartLine"></canvas>
+        <h2>Total Spend Every Month</h2>
+        <canvas id="ChartLine"></canvas>
     </div>
+
 
 </div>
 
