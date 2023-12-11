743a1fa6c7be7d3ed9dd1fbc67d30a577b65fa9c
Matthew Joseph Fang Bilaos <105849780+Mattseph@users.noreply.github.com>
2023-12-11 14:14:04 +0800

    Add: sales-chart.php

---
+++ data-visualization/sales-chart.php
@@ -0,0 +1,15 @@
+<?php
+$pageTitle = 'Sales Charts';
+
+include './../includes/header.php'
+?>
+
+<div class="chartCard">
+
+    <div class="chartBox line">
+        <canvas id="myChartLine"></canvas>
+    </div>
+
+</div>
+
+<?php include './../includes/footer.php' ?>
