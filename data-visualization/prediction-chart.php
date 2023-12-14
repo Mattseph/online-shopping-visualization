89915634b8f5e91f898949ccef4564a015df6c3d
masterdave000 <127076290+masterdave000@users.noreply.github.com>
2023-12-14 13:43:00 +0800

    Edit: 24 months -> 12 months

--- data-visualization/prediction-chart.php
+++ data-visualization/prediction-chart.php
@@ -7,7 +7,7 @@ include './../includes/header.php';
 <div class="chartCard">
 
     <div class="chartBox line">
-        <h2>Predicted Total Spend Per Transacation in the next 24 months</h2>
+        <h2>Predicted Total Spend Per Transacation in the next 12 months</h2>
         <canvas id="clvChart"></canvas>
     </div>
 
