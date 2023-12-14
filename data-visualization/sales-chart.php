e457a34ed7bdc66a42509279d47a1e886f56d0a0
masterdave000 <127076290+masterdave000@users.noreply.github.com>
2023-12-14 11:50:11 +0800

    Add: div with id=cityChartBar, and categoryChartBar

--- data-visualization/sales-chart.php
+++ data-visualization/sales-chart.php
@@ -7,8 +7,20 @@ include './../includes/header.php';
 <div class="chartCard">
 
     <div class="chartBox line">
-        <h2>Total Spend Every Month</h2>
+        <h2>Total Sales Every Month</h2>
         <canvas id="chartLine"></canvas>
+    </div>
+
+    <div class="chartBox bar">
+        <h2>Total Sales Every City</h2>
+
+        <canvas id="cityChartBar"></canvas>
+    </div>
+
+    <div class="chartBox bar">
+        <h2>Total Sales Every Category</h2>
+
+        <canvas id="categoryChartBar"></canvas>
     </div>
 
 </div>
