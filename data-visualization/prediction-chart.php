85760d8fd3a81ee81e09dc3a2cdcc8a8fa04bb89
Matthew Joseph Fang Bilaos <105849780+Mattseph@users.noreply.github.com>
2023-12-19 11:49:28 +0800

    Add: Model Evaluation Metrics div Container

--- data-visualization/prediction-chart.php
+++ data-visualization/prediction-chart.php
@@ -7,11 +7,13 @@ include './../includes/header.php';
 <div class="chartCard">
 
     <div class="chartBox line">
-        <h2>Predicted Total Spend Per Transacation in the next 12 months</h2>
+        <h2 style="text-align: center">Predicted Total Spend Per Transacation in the next 12 months</h2>
         <canvas id="clvChart"></canvas>
     </div>
 
+    <div class="modelMetrics">
 
+    </div>
 </div>
 
 <?php include './../includes/footer.php' ?>
